b2f438c65bded0b8b7dc17054ca6d693780dd402
Carlos Augusto Grispan <cagrispan@gmail.com>
2015-11-14 11:31:23 -0200

    Salarys Class

--- src/DAOs/DepartmentDAO.java
+++ src/DAOs/DepartmentDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAOs;
 
 import entities.Department;
@@ -15,10 +10,6 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import resources.ConnectionFactory;
 
-/**
- *
- * @author TUNTS
- */
 public class DepartmentDAO {
 
     private static final String insert = "INSERT INTO department(name) VALUES(?)";
--- src/entities/Salary.java
+++ src/entities/Salary.java
@@ -1,5 +1,33 @@
 
 package entities;
 public class Salary {
+    private int idOffice;
+    private int level;
+    private float value;
+
+    public int getIdOffice() {
+        return idOffice;
+    }
+
+    public void setIdOffice(int idOffice) {
+        this.idOffice = idOffice;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+    
     
 }
